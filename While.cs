23f484f4e9fb5b9d1f21eec4f69960ffb6416274
JeffPadgett <JeffreyPadgett1@gmail.com>
2018-10-10 19:29:56 -0400

    while and do while

---
+++ While.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("Guess a number: from 1 to 5: ");
+            int guessedNumber = Convert.ToInt32(Console.ReadLine());
+            bool notGuessed = false;
+
+            do
+            {
+
+                switch (guessedNumber)
+                {
+                    case 1:
+                        Console.WriteLine("You guessed 1.. Please guess again");
+                        Console.WriteLine("Guess another number ");
+                        guessedNumber = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+
+                    case 2:
+
+                        Console.WriteLine("you guessed 1..try again");
+                        Console.WriteLine("Guess another number ");
+                        guessedNumber = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+
+                    case 3:
+
+                        Console.WriteLine("You won! You guessed it correctly! ");
+                        notGuessed = true;
+                        break;
+
+                    case 4:
+
+                        Console.WriteLine("you guessed 1..try again");
+                        Console.WriteLine("Guess another number ");
+                        guessedNumber = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 5:
+
+                        Console.WriteLine("you guessed 1..try again");
+                        Console.WriteLine("Guess another number ");
+                        guessedNumber = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+
+                }
+            }
+
+            while (!notGuessed);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
