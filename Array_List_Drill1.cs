3836b36e8c437b4886814170a5f89e5902f19703
JeffPadgett <JeffreyPadgett1@gmail.com>
2018-10-11 20:49:22 -0400

    gogo

---
+++ Array_List_Drill1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        //    int[] testScores = { 91, 96, 93, 61, 47, 18, 100, };
+
+        //    for (int i = 0; i < testScores.Length; i++)
+        //    {
+        //        if (testScores[i] > 85)
+        //        {
+        //        Console.WriteLine("Passing test Score: " + testScores[i]);
+        //        }
+
+
+        //    }
+        //Console.ReadLine();
+
+        //string[] names = { "Jeffrey", "Cletious", "Daniel", "Brittany" };
+
+        //for (int j = 0; j < names.Length; j++)
+        //{
+        //if (names[j] == "Brittany")
+        //{
+        //    Console.WriteLine(names[j]);
+        //}
+        //Console.WriteLine(names[j]);
+
+        //    List<int> testScores = new List<int>();
+        //    testScores.Add(98);
+        //    testScores.Add(93);
+        //    testScores.Add(68);
+        //    testScores.Add(71);
+        //    testScores.Add(89);
+        //    testScores.Add(99);
+
+        //    foreach (int score in testScores)
+        //    {
+        //        if (score > 85)
+        //        {
+        //            Console.WriteLine("Passing test score: " + score);
+        //        }
+        //    }
+        //    Console.ReadLine();
+
+        List<string> names = new List<string>() { "Jesse", "Brittany", "Adam", "Clover", "Jeffrey" };
+
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+         
+        }
+
+        Console.ReadLine();
+
+
+
+    }
+    
+        
+}
+
+
+
+
+
