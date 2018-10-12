e3e44a1c364fef2bc652abcb6570286acff14860
JeffPadgett <JeffreyPadgett1@gmail.com>
2018-10-12 11:06:49 -0400

    drill addition

---
+++ ArrayV2.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+class Program
+{
+
+    static void Main()
+    {
+
+        //string[] arrayOne = { "this is a string", "Another string.", "This is the third string." };
+        //string[] arrayTwo = { "this is a 10", "Another 20.", "This is the 3rd string.", "What is this" };
+        //Console.WriteLine("Please enter something to add to the strings: ");
+        //string stringToAdd = Console.ReadLine();
+
+        //foreach (string randomString in arrayOne)
+        //{
+        //    Console.WriteLine(randomString + " " + stringToAdd);
+        //}
+
+
+        //Console.ReadLine();
+
+        /*--------------An infinite loop*-------------------------*/
+
+        //bool infiniteStarter = true;
+
+        //while (infiniteStarter)
+        //{
+        //    Console.WriteLine("This is an infinite loop");
+        //}
+
+        /*-----------END of infinite LOOP-----------------------*/
+
+        //for (int i = 0; i < arrayOne.Length; i++)
+        //{
+        //    if (arrayOne[i] == "another string.")
+        //    {
+        //        Console.WriteLine("We used the < opperator");
+        //    }
+        //}
+
+        //for (int j = 0; j <= arrayTwo.Length; j++)
+        //    if (arrayTwo[j] == "This is the 3rd string.")
+        //    {
+        //        Console.WriteLine("We used the <= opperator");
+        //    }
+
+
+        /*-----------------------NUMBER 6----------------------------*/
+
+        //List<string> uniqueText = new List<string>() { "Mario", "Dark Souls", "Zelda", "World of Warcraft" };
+        //Console.WriteLine("Please enter either Mario, Dark Souls, Zelda, or World of Warcraft and I will display the index of that element.");
+        //string simularity = Console.ReadLine();
+
+        //if (simularity != uniqueText[0] && simularity != uniqueText[1] && simularity != uniqueText[2] && simularity != uniqueText[3])
+        //{
+        //    Console.WriteLine("You entered in the string name incorrectly.");
+        //}
+
+        //for (int i = 0; i < uniqueText.Count; i++)
+        //{
+        //    if (simularity == uniqueText[i])
+        //    {
+        //        Console.WriteLine("You selected the index: " + i);
+
+        //    }
+
+        //}
+        //Console.WriteLine("Press Enter to Proceed");
+        //Console.ReadLine();
+
+        /*--------------------------END----------------------------------*/
+
+
+        /*-------------------------Number 9-------------------------------*/
+
+        //List<string> secondText = new List<string>() { "Kerby", "Fluid", "Kerby", "Elf" };
+        //Console.WriteLine("Please enter either Kerby, Fluid, or Elf and I will display the index or indices of that element.");
+        //string userInput = Console.ReadLine();
+
+        //if (userInput != secondText[0] && userInput != secondText[1] && userInput != secondText[2] && userInput != uText[3])
+        //{
+        //    Console.WriteLine("You entered in the string name incorrectly.");
+        //}
+
+        //for (int i = 0; i < secondText.Count; i++)
+        //{
+        //    if (userInput == secondText[i])
+        //    {
+        //        Console.WriteLine("You selected the index: " + i);
+
+        //    }
+
+        //}
+        //Console.WriteLine("Press Enter to Proceed");
+        //Console.ReadLine();
+
+        /*--------------------------END----------------------------------*/
+
+
+        /*-------------------------Number 11-------------------------------*/
+        
+    }
+}
---
+++ Drill_10.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+    {
+        static void Main()
+        {
+            List<string> groceryList = new List<string>() { "Onions", "Cucumbers", "Orange Juice", "Onions", "Pineapple", "Pineapple" };
+
+            int index = 0;
+
+            foreach (string grocery in groceryList)
+            {
+            Console.WriteLine(grocery);
+                for (int i = 0; i < groceryList.Count; i++)
+                {
+                Console.WriteLine("        " + groceryList[i]);
+
+                    if (groceryList[i] == grocery && index > i)
+                    {
+                        Console.WriteLine(grocery + " already appears in the list");
+                    }
+                }
+                index++;
+            }
+            Console.ReadLine();
+    }
+    }
+
