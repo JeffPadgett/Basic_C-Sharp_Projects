bed28b7ecdafdd1c45ca1defa0d0930ab20545e5
Jeffrey <JeffreyPadgett1@gmail.com>
2018-10-13 20:36:22 -0700

    Class_Objects introduction

---
+++ Class_Objects/Class_Objects/MathTools.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Objects
+{
+    class MathTools
+    {
+        public static int addSix (int num)
+        {
+            int sum = 6 + num;
+            return sum;
+        }
+
+        public static int multiplyByEight (int num)
+        {
+            int sum = 8 * num;
+            return sum;
+        }
+
+        public static int divideByTwo (int num)
+        {
+            int sum = num / 2;
+            return sum;
+        }
+    }
+}
---
+++ Class_Objects/Class_Objects/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number to perform math operations on");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(MathTools.addSix(userInput));
+            Console.WriteLine(MathTools.multiplyByEight(userInput));
+            Console.WriteLine(MathTools.divideByTwo(userInput));
+            Console.ReadLine();
+
+        }
+    }
+}
