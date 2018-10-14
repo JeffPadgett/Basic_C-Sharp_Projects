0db7a60acebb8301e1fc90a48c5addf7f4678673
JeffPadgett <JeffreyPadgett1@gmail.com>
2018-10-13 20:51:34 -0400

    drill

---
+++ Class_Methods/Class_Methods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            intProcessor tool = new intProcessor();
+
+            tool.addSixAndPrintOther(12, 18);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Class_Methods/Class_Methods/intProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Methods
+{
+    class intProcessor
+    {
+
+        public void addSixAndPrintOther(int input,int displayNumber)
+        {
+            int sum = input + 6;
+            Console.WriteLine("This is the number you chose: " + displayNumber);
+            
+            
+        }
+    }
+}
