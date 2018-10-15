dbf2aed0fac0250bfd453a84e820db3b07d96317
JeffPadgett <JeffreyPadgett1@gmail.com>
2018-10-15 00:04:22 -0400

    drill for output parameters

---
+++ Output_Paramaters/Output_Paramaters/MathTools.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Output_Paramaters
+{
+    class MathTools
+    {
+        public static void DivTwo (int userInput)
+        {
+            
+           int sum = userInput / 2;
+            Console.WriteLine("The result of your entry is: " + sum);
+        }
+
+        public static void DivTwo (int userInput, out int storedNumber)
+        {
+            
+            storedNumber = userInput / 2;
+            Console.WriteLine("The result of your number is: " + storedNumber);
+            
+        }
+    }
+}
---
+++ Output_Paramaters/Output_Paramaters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Output_Paramaters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int savedNumber;
+            Console.WriteLine("Please enter a number. It will be divided by 2.: ");
+            int selectedNumber = Convert.ToInt32(Console.ReadLine());
+            MathTools.DivTwo(selectedNumber,out savedNumber);
+            Console.WriteLine("We are going to store it for you " + savedNumber);
+            Console.ReadLine();
+
+
+        }
+    }
+}
