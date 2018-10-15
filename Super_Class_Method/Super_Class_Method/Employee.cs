2411c186a35fa4aa6feec17088bd675afba5ea10
JeffPadgett <JeffreyPadgett1@gmail.com>
2018-10-15 02:27:23 -0400

    Super_Class_Methods

---
+++ Super_Class_Method/Super_Class_Method/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Super_Class_Method
+{
+    class Employee : Person
+    {
+
+       public Employee(string fName, string lName)
+        {
+            this.firstName = fName;
+            this.lastName = lName;
+        }
+        public int id { get; set; }
+    }
+}
---
+++ Super_Class_Method/Super_Class_Method/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Super_Class_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person bob = new Person(); //test
+
+            Employee Brittany = new Employee("Sample","Student");
+
+            Console.WriteLine(Brittany.firstName);
+
+            Brittany.SayName();
+            Console.ReadLine();
+        }
+    }
+}
