ebdcd61a8fd54d30ee74d4bf2cdafd4fc9886a02
JeffPadgett <JeffreyPadgett1@gmail.com>
2018-10-14 21:56:55 -0400

    optional para drill

---
+++ Overloading_Drill1/Overloading_Drill1/MathObjects.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Drill1
+{
+    class MathObjects
+    {
+        public static void AddUpToFourNums(int x, int y = 0, int z = 0, int a = 0)
+        {
+            int sum = x + y + z + a;
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Overloading_Drill1/Overloading_Drill1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Drill1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathObjects.AddUpToFourNums(5, 3, 2, 1);
+            MathObjects.AddUpToFourNums(2, 19);
+            MathObjects.AddUpToFourNums(9);
+            MathObjects.AddUpToFourNums(19903, 16);
+            Console.ReadLine();
+        }
+    }
+}
