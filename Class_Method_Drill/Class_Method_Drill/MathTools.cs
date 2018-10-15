a5ce48ec8642caa1fba6361e520f77e6c0243e76
JeffPadgett <JeffreyPadgett1@gmail.com>
2018-10-14 20:55:42 -0400

    drill update

---
+++ Class_Method_Drill/Class_Method_Drill/MathTools.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Drill
+{
+    class MathTools
+    {
+        public int MultiplyByThree(int input)
+        {
+            int sum = input * 3;
+            return sum;
+
+        }
+
+        public int MultiplyByThree(decimal input)
+        {
+
+            int sum = Convert.ToInt32(input / 3);
+            return sum;
+        }
+
+        public int MultiplyByThree(string input)
+        {
+            int convertedString = Convert.ToInt32(input);
+            int sum = convertedString;
+            return sum;
+
+        }
+    }
+}
---
+++ Class_Method_Drill/Class_Method_Drill/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathTools variableNumber = new MathTools();
+            int storedNumber = 12;
+            storedNumber = variableNumber.MultiplyByThree(storedNumber);
+            Console.WriteLine(storedNumber);
+            Console.ReadLine();
+
+            MathTools decNumber = new MathTools();
+            decimal storedDec = 12.20m;
+            storedDec = decNumber.MultiplyByThree(storedDec);
+            Console.WriteLine(storedDec);
+            Console.ReadLine();
+
+            MathTools addSix = new MathTools();
+            string stringNumber = "10";
+            int processedNumber;
+            processedNumber = addSix.MultiplyByThree(stringNumber);
+            Console.WriteLine(processedNumber);
+            Console.ReadLine();
+        }
+    }
+}
