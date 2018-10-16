4a1e7d007a02a0ccb2c5a1f9b1454b1a517cad46
Jeffrey <JeffreyPadgett1@gmail.com>
2018-10-16 08:23:07 -0700

    generics

---
+++ Generic_Properties/Generics/Abstract_Classes/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes
+{
+    class Employee<T> : Person, IQuittable
+    {
+        public int employeeID { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Hello, my name is {0} {1}", firstName, lastName); 
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("This employee has quit, their information is now removed" );
+            this.firstName = "";
+            this.lastName = "";
+        }
+
+        /*---------------GENERICS---------------*/
+
+        public List<T> things;
+
+
+
+
+
+
+    }
+}
---
+++ Generic_Properties/Generics/Abstract_Classes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Employee<string> employee2 = new Employee<string>();
+            employee2.firstName = "Harry";
+            employee2.lastName = "soulis";
+            employee2.employeeID = 1462;
+            employee2.things = new List<string>() { "badge ", "coffee cup ", "comb " };
+
+            Employee<int> employee3 = new Employee<int>();
+            employee3.firstName = "Brian";
+            employee3.lastName = "Ducky";
+            employee3.employeeID = 1462;
+            employee3.things = new List<int>() { 10, 241, 9 };
+
+            foreach (string thing in employee2.things)
+            {
+                Console.WriteLine("I brought my " + thing + "to work");
+            }
+
+            foreach (int thing in employee3.things)
+            {
+                Console.WriteLine("Number of things: " + thing);
+            }
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
