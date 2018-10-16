d0711d9f383e4f7a4fb6df6715dba6432d552809
Jeffrey <JeffreyPadgett1@gmail.com>
2018-10-15 20:43:20 -0700

    abstract_class

---
+++ Abstract_Classes/Abstract_Classes/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes
+{
+    class Employee : Person
+    {
+
+        public override void SayName()
+        {
+            Console.WriteLine("Hello, my name is {0} {1}", firstName, lastName); 
+        }
+    }
+}
---
+++ Abstract_Classes/Abstract_Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            employee1.firstName = "Sample";
+            employee1.lastName = "Student";
+            employee1.SayName();
+            Console.ReadLine();
+        }
+    }
+}
