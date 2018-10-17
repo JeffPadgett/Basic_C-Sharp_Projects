01a35e801fdff40966406aa0001ef13e7486335c
JeffPadgett <JeffreyPadgett1@gmail.com>
2018-10-17 03:50:12 -0400

    struct

---
+++ Struct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+
+        public struct BigMoney
+        {
+            public decimal amount;
+        }
+
+        static void Main(string[] args)
+        {
+            BigMoney bank = new BigMoney();
+            bank.amount = 100m;
+        }
+    }
+}
