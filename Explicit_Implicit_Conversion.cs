f79722dd0451a84566fb59b71acf7ab6472ab996
Jeffrey <JeffreyPadgett1@gmail.com>
2018-10-23 04:01:45 -0700

    Conversion Exersise

---
+++ Explicit_Implicit_Conversion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implicit_Explicit_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // implicit conversion
+            int num = 121324;
+            long bigNum = num;
+
+            float myFloat = 13.37f;
+            double myNewDouble = myFloat;
+
+            //------------------------------------
+
+
+            double myDouble = 13.37;
+            int myInt;
+
+            // cast double to int;
+
+            myInt = (int)myDouble; //Explicit Conversion!
+
+            //---------------------------------------
+
+            //typeConversion
+            string myString = myDouble.ToString(); //"13.37"   You can do this with doubles, ints, longs, so forth. 
+            string challengeString = myFloat.ToString();
+            bool sunIsShining = false;
+            string myBoolString = sunIsShining.ToString();
+
+
+
+
+
+
+
+        }
+    }
+}
