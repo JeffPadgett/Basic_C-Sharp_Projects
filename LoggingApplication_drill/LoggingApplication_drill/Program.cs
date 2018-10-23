c0c16ea51c6803b7cde29d13200680632a527f8d
Jeffrey <JeffreyPadgett1@gmail.com>
2018-10-23 10:25:15 -0700

    text_log

---
+++ LoggingApplication_drill/LoggingApplication_drill/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LoggingApplication_drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int numberToLog = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\jp171\Documents\visual studio 2015\Projects\LoggingApplication_drill\LoggingApplication_drill\Logs\log.txt", true))
+            {
+                file.WriteLine(numberToLog);
+            }
+            Console.ReadLine();
+        }
+    }
+}
