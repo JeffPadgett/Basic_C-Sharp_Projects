005b2762fe556d1cd656e9ca4602aa5a738744c4
Jeffrey <JeffreyPadgett1@gmail.com>
2018-10-25 05:40:09 -0700

    newest drill

---
+++ Additional_Features/Additional_Features/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features
+{
+    class Program
+    {
+        public class Human
+        {
+            const string sex = "male";
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string HairColor { get; set; }
+
+            public Human(string name, int age, string hairColor)
+            {
+                Age = age;
+                Name = name;
+                HairColor = hairColor;
+            }
+
+            public Human(string name) : this(name, 50, "brown")
+            {
+
+            }
+
+            public Human(int age) : this("bob", age, "brown")
+            {
+
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            var ceo = new Human("Henry");
+            Console.WriteLine(ceo.Name + " " + ceo.Age + " " + ceo.HairColor);
+            Console.ReadLine();
+
+        }
+    }
+}
