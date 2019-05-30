5d9cc530139746efc35b8ae8b595a49b1a369d68
JeffPadgett <JeffreyPadgett1@gmail.com>
2019-05-30 00:29:01 -0400

    simple gradebook application .NET core for larger project

---
+++ Gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grades = new List<double>() {38.48, 42.4, 2.3};
+            grades.Add(56.1);
+
+            var result = 0.0;
+            foreach(var number in grades)
+            {
+                result += number;
+            }
+
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
+
+            if(args.Length > 0)
+            {
+            Console.WriteLine($"Hello, {args[0]}");
+            }
+            else 
+            {
+                Console.WriteLine("Hello!");
+            }
+        }
+    }
+}
